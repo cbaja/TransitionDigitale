f6979dc967da3d581c49a1601d653701f5ce2ed9
= <=>
2017-09-30 14:03:19 -0400

    last change

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController) {
+  thePost :any;
+  constructor(public navCtrl: NavController, public navParams :NavParams ) {
+ 
+   this.thePost=navParams.get("thePost");
 
   }
 
--- src/pages/documentation/documentation.ts
+++ src/pages/documentation/documentation.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController } from 'ionic-angular';
 import { AboutPage } from '../about/about';
 import {PeopleServiceProvider} from '../../providers/people-service/people-service';
-import { Http } from '@angular/http';
+// import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-documentation',
@@ -12,7 +12,7 @@ import { Http } from '@angular/http';
 })
 export class DocumentationPage {
   postList : any ;//= any;
-  constructor(public navCtrl: NavController, private http: Http,
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController, //private http: Http, 
     public peopleServiceProvider: PeopleServiceProvider) {
     this.getPosts();
   }
@@ -26,5 +26,38 @@ export class DocumentationPage {
   goToDocument(){
     this.navCtrl.push(AboutPage);
   }
- 
+  showdetails(thePost){
+
+    //let index = this.postList.indexOf(thePost);
+    this.navCtrl.push(AboutPage,{
+      thePost:thePost
+    });
+    //alert(thePost)
+    /*
+    let prompt = this.alertCtrl.create({
+      title: 'Modifier informations',
+      inputs: [{
+          name: 'title'
+      }],
+      buttons: [
+          {
+              text: 'Cancel'
+          },
+          {
+              text: 'Save',
+              handler: data => {
+                  let index = this.postList.indexOf(thePost);
+
+                  if(index > -1){
+                    this.postList[index] = data;
+                  }
+              }
+          }
+      ]
+  });
+
+  prompt.present();   
+  */
+  }
+  
 }
